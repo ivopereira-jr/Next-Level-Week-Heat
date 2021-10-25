b6879bc4435802d3f74a8f50170a429a42966290
Ivo Pereira <ivopereira.jr12@gmail.com>
2021-10-25 00:02:14 -0300

    :tada: styles home

---
+++ app/src/screens/Home/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+import { COLORS } from '../../theme';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: getStatusBarHeight() + 17,
+    backgroundColor: COLORS.BLACK_SECONDARY
+  }
+});
